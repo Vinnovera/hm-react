516984c617dab10ddc0b48bcbc19f35339116166
jakobohman <jakob@vinnovera.se>
2018-05-22 19:49:15 +0200

    Latest

--- src/App.js
+++ src/App.js
@@ -3,25 +3,41 @@ import './App.css';
 
 class App extends Component {
 	state = {
-		counter: 0
+		counter: 0,
+		input: 'bleh',
+		valid: true
 	}
 
 	decreaseCounter = () => this.setState({ counter: this.state.counter - 1 });
 	increaseCounter = () => this.setState({ counter: this.state.counter + 1 });
 
-	getStyles = () => ({
-		color: this.props.redButton ? 'red' : 'black'
-	})
+	handleInputChange = e => {
+		const value = e.target.value;
+
+
+		this.setState({
+			input: value,
+			valid: value === 'hey'
+		});
+	}
 
 	render() {
 		return (<Fragment>
-			<button onClick={this.decreaseCounter} style={this.getStyles()}>-</button>
+			<Button onClick={this.increaseCounter}>+</Button>
 			<Counter number={this.state.counter} />
-			<button onClick={this.increaseCounter} style={this.getStyles()}>+</button>
+			<Button onClick={this.decreaseCounter}>-</Button>
+			<Input value={this.state.input} onChange={this.handleInputChange} valid={this.state.valid} />
+			{this.state.input}
 		</Fragment>);
 	}
 }
 
-const Counter = (props) => <span>{props.number}</span>;
+const Button = ({ onClick, children }) => <button onClick={onClick}>{children}</button>;
+
+const Counter = ({ number }) => <span>{number}</span>;
+
+const Input = ({ value, onChange, valid }) => <input type="text" value={value} onChange={onChange} style={{
+	color: valid ? 'black' : 'red'
+}}/>;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,3 +6,40 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<div><App redButton /><App /></div>, document.getElementById('root'));
 registerServiceWorker();
+
+/*
+function foo(arg1, arg2) {
+	return 'bar';
+}
+
+var foo = function(arg1, arg2) {
+	return 'bar';
+}
+
+// Arrow functions
+var subtract = (num1 = 1, num2 = 2) => num1 - num2;
+
+suctract(undef);
+
+// New cariables
+const foo = 'bar';
+let foo = 'bar';
+
+() => {
+	if (true) {
+		let foo = '';
+	}
+
+}
+
+// Destructuring
+const foo = {
+	key1: { subKey1: 'bar'},
+	key2: 'baz'
+}
+
+const { key1, key2 } = foo;
+
+const bar = ({ key1 }) => key1;
+
+bar(foo);*/
